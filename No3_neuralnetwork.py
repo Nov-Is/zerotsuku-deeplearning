870b37a6aa1be56c6bbdaa168f05b48c23fbd0f8
Nov-Is <jza80.na2@gmail.com>
2026-06-14 15:42:26 +0900

    第3章 ニューラルネットワークの順方向の伝播(forward propagation)

--- No3_neuralnetwork.py
+++ No3_neuralnetwork.py
@@ -249,3 +249,85 @@ img = img.reshape(28, 28)
 print(img.shape)
 
 img_show(img)
+
+# 3.6.2 ニューラルネットワークの推論処理
+import sys, os
+import pickle
+sys.path.append(os.pardir)
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    pkl_path = os.path.join(base_dir, "deep-learning-from-scratch", "ch03", "sample_weight.pkl")
+    if not os.path.exists(pkl_path):
+        pkl_path = os.path.join(os.getcwd(), "deep-learning-from-scratch", "ch03", "sample_weight.pkl")
+    with open(pkl_path, "rb") as f:
+        network = pickle.load(f)
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+
+x, t = get_data()
+network = init_network()
+
+accuracy_cnt = 0
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y) # 最も確率の高い要素のインデックスを取得
+    
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
+# 3.6.3 バッチ処理
+x, _ = get_data()
+network = init_network()
+W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+
+x.shape
+x[0].shape
+W1.shape
+W2.shape
+W3.shape
+
+# バッチ処理の実装
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1) # axisとはy_batch.shape(100, 10)のどの次元でargmaxを取得するかの指定。今回は0ではなく1次元の方向に沿って最大を取得したいのでこの指定
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+    
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
+
+### axisの例
+x = np.array([[0.1, 0.8, 0.1], [0.3, 0.1, 0.6], [0.2, 0.5, 0.3], [0.8, 0.1, 0.1]])
+y = np.argmax(x, axis=1)
+
+y = np.array([1, 2, 1, 0])
+t = np.array([1, 2, 0, 0])
+print(y==t)
+
+np.sum(y==t)
